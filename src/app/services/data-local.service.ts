2e2082409289819f9769346a3a3b95413653e512
Jonathan <jonathan962911@gmail.com>
2020-02-01 15:48:50 -0500

    Agregando un toast y algoritmo para verificar si la noticia ya exites en favoritos

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { PeliculaDetalle } from 'src/app/interfaces/peliculaDetalleinterface';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,39 @@ export class DataLocalService {
   public peliculas: PeliculaDetalle[] = [];
 
   constructor(
-    private storage: Storage
+    private storage: Storage,
+    private toastCtrl: ToastController
   ) { }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1500
+    });
+    toast.present();
+  }
 
 
   guardarPelicula(pelicula: PeliculaDetalle) {
-    this.peliculas.push(pelicula);
+    let existe: boolean = false;
+    let mensaje: string = '';
 
+    for (const peli of this.peliculas) {
+      if (peli.id === pelicula.id) {
+        existe = true;
+        break;
+      }
+    }
+
+    if (existe) {
+      this.peliculas.filter(peli => peli.id !== pelicula.id);
+      mensaje = 'Pelicula eliminada de favoritos';
+    } else {
+      this.peliculas.push(pelicula);
+      mensaje = 'Pelicula agregada a favoritos';
+
+    }
+    this.presentToast(mensaje);
     this.storage.set('peliculas', this.peliculas);
-
   }
 }
